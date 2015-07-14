d517a3206d7d51c63a6eb541bf29b036c8db3d0a
Kelvin Nascimento Gueriero <kelvin_n_gueriero@yahoo.com.br>
2015-07-14 12:06:25 -0300

    Primeiro commit

---
+++ balloonploc/config.lua
@@ -0,0 +1,33 @@
+application =
+{
+
+	content =
+	{
+		graphicsCompatibility = 1,
+		width = 320,
+		height = 480, 
+		scale = "zoomStretch",
+		fps = 30,
+		
+		--[[
+		imageSuffix =
+		{
+			    ["@2x"] = 2,
+		},
+		--]]
+	},
+
+	--[[
+	-- Push notifications
+	notification =
+	{
+		iphone =
+		{
+			types =
+			{
+				"badge", "sound", "alert", "newsstand"
+			}
+		}
+	},
+	--]]    
+}
