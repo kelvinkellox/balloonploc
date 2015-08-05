07ae34b11c061b70e2d22e0d60a88e4a11273718
Kelvin Nascimento Gueriero <kelvin_n_gueriero@yahoo.com.br>
2015-08-05 03:11:32 -0300

    Modifies config class

--- balloonploc/config.lua
+++ balloonploc/config.lua
@@ -1,33 +1,92 @@
-application =
-{
+if ( string.sub( system.getInfo("model"), 1, 4 ) == "iPad" ) then
+   application =
+   {
+      content =
+      {
+         width = 360,
+         height = 480,
+         scale = "letterBox",
+         xAlign = "center",
+         yAlign = "center",
+         imageSuffix =
+         {
+            ["@2x"] = 1.5,
+            ["@4x"] = 3.0,
+         },
+      },
+   }
 
-	content =
-	{
-		graphicsCompatibility = 1,
-		width = 320,
-		height = 480, 
-		scale = "zoomStretch",
-		fps = 30,
-		
-		--[[
-		imageSuffix =
-		{
-			    ["@2x"] = 2,
-		},
-		--]]
-	},
+   elseif ( string.sub( system.getInfo("model"), 1, 2 ) == "iP" and display.pixelHeight > 960 ) then
+   application =
+   {
+      content =
+      {
+         width = 320,
+         height = 568,
+         scale = "letterBox",
+         xAlign = "center",
+         yAlign = "center",
+         imageSuffix =
+         {
+            ["@2x"] = 1.5,
+            ["@4x"] = 3.0,
+         },
+      },
+   }
 
-	--[[
-	-- Push notifications
-	notification =
-	{
-		iphone =
-		{
-			types =
-			{
-				"badge", "sound", "alert", "newsstand"
-			}
-		}
-	},
-	--]]    
-}
+
+elseif ( string.sub( system.getInfo("model"), 1, 2 ) == "iP" ) then
+   application =
+   {
+      content =
+      {
+         width = 320,
+         height = 480,
+         scale = "letterBox",
+         xAlign = "center",
+         yAlign = "center",
+         imageSuffix =
+         {
+            ["@2x"] = 1.5,
+            ["@4x"] = 3.0,
+         },
+      },
+   }
+
+   elseif ( display.pixelHeight / display.pixelWidth > 1.72 ) then
+   application =
+   {
+      content =
+      {
+         width = 320,
+         height = 570,
+         scale = "letterBox",
+         xAlign = "center",
+         yAlign = "center",
+         imageSuffix =
+         {
+            ["@2x"] = 1.5,
+            ["@4x"] = 3.0,
+         },
+      },
+   }
+
+else
+   application =
+   {
+      content =
+      {
+         width = 320,
+         height = 512,
+         scale = "letterBox",
+         xAlign = "center",
+         yAlign = "center",
+         imageSuffix =
+         {
+            ["@2x"] = 1.5,
+            ["@4x"] = 3.0,
+         },
+      },
+   }
+
+end
