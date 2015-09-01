2eb95225924f72d8252dc327ec7c12a7fff55e3b
Kelvin Nascimento Gueriero <kelvin_n_gueriero@yahoo.com.br>
2015-09-01 08:11:08 -0300

    update balloon class and add balloon color

--- balloonploc/balloon.lua
+++ balloonploc/balloon.lua
@@ -1,32 +1,49 @@
 -----------------------------
 -- balloon.lua 
 -----------------------------
+--require Constants
+local C = require("constants" )
+
 local balloon = {}
 local balloon_mt = {__index = balloon} -- metatable
 	
 
-	function balloon:new( posX, posY, color) -- constructor
+	function balloon:new( posX, posY, colorNum) -- constructor
 
 		local newBalloon = {}
 		--Display Image and set color
-		newBalloon.image = display.newImage("Images/balaoteste.png")
-		newBalloon.bColor = color
+		newBalloon.image = display.newImage(C.IMG_BALLOON)
+		newBalloon.image.bColor = colorNum
 
-		-- Set position and physics of object
-		physics.addBody( newBalloon.image, { density=2.0, friction=0, bounce=0, radius=15 })
+		--Tint balloon
+		local function Blue()
+			newBalloon.image:setFillColor( 0,0,255 )
+		end
+		local function Red()
+			newBalloon.image:setFillColor( 255,0,0 )
+		end
+		local function Green()
+			newBalloon.image:setFillColor( 0,255,0 )
+		end
+		local function Yellow()
+			newBalloon.image:setFillColor( 255,255,0 )
+		end
+
+		local color = {Blue,Red,Green,Yellow}
+		tintBalloon = color[colorNum]
+		tintBalloon()
+
+		-- Set position of object
 		newBalloon.image.x = posX
 		newBalloon.image.y = posY
-		newBalloon.image.gravityScale = -0.10
 
 
 		setmetatable( newBalloon, balloon_mt )
 		return newBalloon
 	end
 
-	-- Remove Display from object and return nil
-	function balloon:rmvBalloon(obj)
-		obj.image:removeSelf( )
-		return nil
-	end
+
+
+
 
 return balloon
--- balloonploc/constants.lua
+++ balloonploc/constants.lua
@@ -1,5 +1,25 @@
 --Constants Space
 local C = {}
+	--Balloon Colors
+	C.STR_TABLE_BALLOON_COLOR = {}
+	C.STR_TABLE_BALLOON_COLOR[1] = "Blue"
+	C.STR_TABLE_BALLOON_COLOR[2] = "Red"
+	C.STR_TABLE_BALLOON_COLOR[3] = "Green"
+	C.STR_TABLE_BALLOON_COLOR[4] = "Yellow"
+
+	--Images
+	C.IMG_BACKGROUND = "Images/sky.png" --Background
+	C.IMG_MENU_BUTTON ="Images/button.png" -- Menu Buttons
+	C.IMG_BALLOON = "Images/balloon.png" -- Balloon
+
+	--Strings / Labels
+	C.STR_BUTTON_PLAY = "Play"
+	C.STR_BUTTON_CREDITS = "Credits"
+	C.STR_BUTTON_CONFIG = "Config"
+
+	--Scales
+	C.RSC_MENU_BUTTON_SCALE_X = 200
+	C.RSC_MENU_BUTTON_SCALE_Y = 40
 
 
 return C
